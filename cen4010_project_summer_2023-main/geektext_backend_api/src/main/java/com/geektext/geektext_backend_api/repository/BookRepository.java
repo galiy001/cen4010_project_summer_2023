9f5fa919c16cd3761bb340fd8acd61a2769974b6
galiy001 <134160039+galiy001@users.noreply.github.com>
2023-06-24 16:03:16 -0400

    Delete cen4010_project_summer_2023-main/geektext_backend_api directory

--- cen4010_project_summer_2023-main/geektext_backend_api/src/main/java/com/geektext/geektext_backend_api/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.geektext.geektext_backend_api.repository;
-
-import com.geektext.geektext_backend_api.entity.BookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<BookEntity, Long> {
-}
