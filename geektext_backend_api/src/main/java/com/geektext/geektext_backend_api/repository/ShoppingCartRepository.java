86d8482b1645abfd7b02d6197a84bdfff1fecb83
heidar <galiy001@fiu.edu>
2023-06-24 15:49:41 -0400

    Revert "Merge pull request #1 from galiy001/geidar_sprint_2_feature_3"
    This reverts commit cf4dc964b5c4355547e92308f73b755a0842b0c2, reversing
    changes made to 09abbde078b552f38ffb0fefe498f7727e7e7c3f.

--- geektext_backend_api/src/main/java/com/geektext/geektext_backend_api/repository/ShoppingCartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.geektext.geektext_backend_api.repository;
-
-import com.geektext.geektext_backend_api.entity.ShoppingCartEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCartEntity, Long> {
-
-}
