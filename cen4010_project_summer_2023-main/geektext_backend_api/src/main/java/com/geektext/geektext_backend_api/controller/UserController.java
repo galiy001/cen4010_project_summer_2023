34b92d50d355f2dfd8bc5f7a26b2c1e1ca4c35fd
Enriquealf99 <122303328+Enriquealf99@users.noreply.github.com>
2023-06-24 15:48:26 -0400

    Add files via upload

---
+++ cen4010_project_summer_2023-main/geektext_backend_api/src/main/java/com/geektext/geektext_backend_api/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.geektext.geektext_backend_api.controller;
+
+import com.geektext.geektext_backend_api.entity.UserEntity;
+import com.geektext.geektext_backend_api.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<UserEntity> returnAllUsers() {
+        return userService.returnAllUsers();
+    }
+
+    @GetMapping("/{user_id}")
+    public Optional<UserEntity> searchByUserId(@PathVariable("user_id") Long user_id) {
+        return userService.searchByUserId(user_id);
+    }
+
+    @PostMapping
+    public UserEntity createNewUser(@RequestBody UserEntity userEntity) {
+        return userService.createNewUser(userEntity);
+    }
+
+    @PutMapping
+    public UserEntity updateUser(@RequestBody UserEntity userEntity) {
+        return userService.updateUser(userEntity);
+    }
+
+    @DeleteMapping("/{user_id}")
+    public void deleteUser(@PathVariable("user_id") Long user_id) {
+        userService.deleteUser(user_id);
+    }
+
+}
